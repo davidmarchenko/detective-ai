108f8ab028f7ae31a3d97fdb88c6093fe721fccd
David Marchenko <davidmarchenko49@gmail.com>
2026-04-02 16:17:15 -0400

    Initial Commit

---
+++ Runway Characters/Runway_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Runway_CharactersApp.swift
+//  Runway Characters
+//
+//  Created by David Marchenko on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Runway_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
